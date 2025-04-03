00e6a417541e95fd1deb3215cef0407f7eb52400
zanellig <gonzalozanelli1@gmail.com>
2025-04-03 14:10:06 -0300

    refactor(attendees): remove console logs from updateAttendeesAssisted function for cleaner code

--- lib/actions/attendees.actions.ts
+++ lib/actions/attendees.actions.ts
@@ -7,21 +7,15 @@ export const updateAttendeesAssisted = async ({
 }: {
   userId: string;
 }) => {
-  console.log("USER ID :_______", userId);
   const idParam = prepareIdParam(userId);
   try {
     const checkAssistedQuery = transformQuery("SELECT assisted from attendees WHERE id = ?");
     const [assistedRows] = await db.query(checkAssistedQuery, [idParam]);
     const assistedResult: boolean = assistedRows[0]?.assisted;
-    console.log("Assisted Result:", assistedResult);
     const updateAssistedQuery = transformQuery(
       `UPDATE attendees SET assisted = ${!assistedResult} WHERE id = ?`
     );
-    console.log("Prepared query asissted:", updateAssistedQuery);
-    
     const [attendeeRows] = await db.query(updateAssistedQuery, [idParam]);
-    console.log("Assisted Result:", attendeeRows[0]);
-
   } catch (error) {
     console.error("Error confirming attendance:", error);
   }
